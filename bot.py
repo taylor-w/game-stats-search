6938246491a774245a9ec1cc6e6a6bd7033e3ada
taylor-w <taylorwood39@gmail.com>
2022-11-17 23:30:19 -0600

    Connect player_search script to command performed by discord bot in bot.py file

--- bot.py
+++ bot.py
@@ -1,28 +1,38 @@
+# imports
 import constants
-print('test1')
 import player_search
-print('test2')
 import discord
+import json
+from discord import app_commands
 from discord.ext import commands
 
 # input: local vars
 intents = discord.Intents.default()
 intents.message_content = True
 bot_key = constants.discord_bot_key
-
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
+# process: declare async command function
+@tree.command(name="apex_search", description="Search multiplayer stats for Apex Legends", guild=discord.Object(id=constants.discord_guild_id))
+async def apex_search(ctx: discord.Interaction, player: str):
+    if not (player):
+        await ctx.respond.send_message('No player entered; try again with entry of player name.')
+    else:
+        results = player_search.main(player)
+        print(type(results))
+        print(len(results))
+        for res in results:
+            title = res
+            description = results[res]
+            description = str(description).replace("{","").replace("}","")
+            em=discord.Embed(title=title, description=description)
+            await ctx.channel.send(embed=em)
+
+# process: declare async ready function
 @client.event
 async def on_ready():
     print(f'Bot has logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('apex'):
-        player_search.main()
-        await message.channel.send('warmer')
+    await tree.sync(guild=discord.Object(id=constants.discord_guild_id))
 
 client.run(bot_key)
